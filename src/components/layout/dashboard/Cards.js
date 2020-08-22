b85427aad673cdf046049cad30b3ef3b179b37d9
inFamousxD <audimankar@gmail.com>
2020-08-22 13:25:45 +0530

    Debugged send request on wishlist

--- src/components/layout/dashboard/Cards.js
+++ src/components/layout/dashboard/Cards.js
@@ -7,7 +7,16 @@ import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../../actions/profile';
 import axios from 'axios';
 
-const Cards = ({ warehouse, auth: { user } }) => {
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+const Cards = ({ warehouse, auth: { user, isAuthenticated } }) => {
     const [wishlisted, setWishlisted] = React.useState(true);
     const handleWishlisted = () => {
         try {
@@ -26,6 +35,39 @@ const Cards = ({ warehouse, auth: { user } }) => {
         }
         setWishlisted(false)
     }
+
+    const autoFill = () => {
+        console.log(user.name)
+        setFormData({
+            name: user[0].name,
+            email: user[0].identity,
+            phone: user[0].contact
+        })
+    }
+
+    const [open, setOpen] = React.useState(false);
+    const handleClickOpen = () => {
+        isAuthenticated && autoFill();
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const initialFormData = {
+        name:user[0].name,
+        email:user[0].identity,
+        phone:user[0].contact
+    }
+    const [formData, setFormData] = React.useState(initialFormData)
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
+    const onChange = e => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+    }
+
     return (
         <Fragment>
             <Card style={{ marginBottom: '1.5rem', marginLeft: '1.5rem', marginRight: '1.5rem' }}>
@@ -69,7 +111,7 @@ const Cards = ({ warehouse, auth: { user } }) => {
                                     <div style={{ fontSize: '13px' }}> Monthly Rental </div> <div style={{ fontSize: '17px', color: '#2a55ae', fontWeight: 'bold' }}> { warehouse.warehouseDetails.pricing } / sq. ft.  </div>
                                 </Col>
                                 <Col sm={3}>
-                                    <Button variant='primary' style={{ width: '100%', height: '100%', backgroundColor: '#2a55ae', border: '0px' }}>
+                                    <Button onClick={handleClickOpen} variant='primary' style={{ width: '100%', height: '100%', backgroundColor: '#2a55ae', border: '0px' }}>
                                         Send Request
                                     </Button>
                                 </Col>
@@ -92,6 +134,58 @@ const Cards = ({ warehouse, auth: { user } }) => {
                     </Row>
                 </Card.Body>
             </Card>
+            <div>
+                <Dialog fullScreen={fullScreen} open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+                    <DialogTitle id="form-dialog-title">Send Request</DialogTitle>
+                    <DialogContent>
+                    <DialogContentText>
+                        You are currently requesting for '<b>{warehouse.warehouseDetails.name}</b>'. Please fill out the
+                        details below and we will get back to you shortly.
+                    </DialogContentText>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label="Name"
+                        type="name"
+                        name="name"
+                        value={formData.name}
+                        fullWidth
+                        onChange={e => onChange(e)}
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label="Email Address"
+                        type="email"
+                        name="email"
+                        value={formData.email}
+                        fullWidth
+                        onChange={e => onChange(e)}
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="Contact"
+                        label="Contact"
+                        type="String"
+                        name="phone"
+                        value={formData.phone}
+                        fullWidth
+                        onChange={e => onChange(e)}
+                    />
+                    </DialogContent>
+                    <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={handleClose} color="primary">
+                        Send Request
+                    </Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
         </Fragment>
     )
 }
